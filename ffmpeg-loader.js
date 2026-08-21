0aa8a065d8b9026d2dc85ca507c73c69beac2477
Alex-97690 <rogachyovaleksandr@gmail.com>
2026-08-21 11:03:48 -0400

    Initialize AlexCut FFmpeg loader module

--- ffmpeg-loader.js
+++ ffmpeg-loader.js
@@ -1,1 +1,8 @@
-console.log("AlexCut FFmpeg loader готов");
+window.AlexCutFFmpeg = {
+    ready: false,
+
+    init: function() {
+        console.log("AlexCut: модуль экспорта загружен");
+        this.ready = true;
+    }
+};
